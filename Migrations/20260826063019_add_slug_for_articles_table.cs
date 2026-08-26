fce85b119b03ed60de954341f10b19559e97c804
moshoubash <ma016188@gmail.com>
2026-08-26 10:01:37 +0300

    test

---
+++ Migrations/20260826063019_add_slug_for_articles_table.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotnetAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_slug_for_articles_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Articles",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: true,
+                computedColumnSql: "LOWER(REPLACE(Title, ' ', '-'))",
+                stored: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Articles");
+        }
+    }
+}
